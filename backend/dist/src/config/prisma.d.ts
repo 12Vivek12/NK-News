e94f5decfeb80a09fa71e7b43e82960351829788
kkaivoiceagent <admin@thanosvsstark@gmail.com>
2026-03-15 13:42:12 +0530

    Update .gitignore and clean up repository

--- backend/dist/src/config/prisma.d.ts
+++
@@ -1,3 +0,0 @@
-declare const prisma: import("../generated-client/internal/class.js").PrismaClient<never, import("../generated-client/internal/prismaNamespace.js").GlobalOmitConfig | undefined, import("@prisma/client/runtime/client").DefaultArgs>;
-export default prisma;
-//# sourceMappingURL=prisma.d.ts.map
--- backend/dist/src/generated-client/models.d.ts
+++
@@ -1,8 +0,0 @@
-export type * from './models/User.js';
-export type * from './models/Category.js';
-export type * from './models/Tag.js';
-export type * from './models/Article.js';
-export type * from './models/Comment.js';
-export type * from './models/Advertisement.js';
-export type * from './commonInputTypes.js';
-//# sourceMappingURL=models.d.ts.map
